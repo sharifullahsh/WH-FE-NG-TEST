c5c3d86829327318556a198fadb3e79dba352464
Sharif <sharifullahsh@gmail.com>
2021-02-22 18:21:00 +0430

    first commit

---
+++ src/app/test-01.ts
@@ -0,0 +1,35 @@
+/**
+ * In the following component, update the code so that when the value of the [loan-amount] is changed:
+ * * If it's blank or 0, the values of [monthly_payment] and [late_payment] becomes "N/A",
+ * * If it has a value, the value of [monthly_payment] becomes 2% of [loan-ammount] and the value of [late_payment] becomes 5% of [monthly_payment].
+ * * Both [monthly_payment] and [late_payment] should print in the template in currency format : $1,234
+ */
+
+import { Component, Input,NgModule  } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+@Component({
+    selector : 'ng-app',
+    templateUrl: './test1/test1.component.html',
+    styleUrls: ['./test1/test1.component.scss']
+                
+})
+export class Test01Component {
+
+    loan_amount:number = 1000;
+    monthly_payment:number = 200;
+    late_payment = 10;
+}
+
+@NgModule({
+    imports : [
+        RouterModule.forChild([
+            {
+                path : "",
+                component : Test01Component
+            }
+        ])
+    ],
+    declarations : [Test01Component]
+})
+export class Test01Module {}
